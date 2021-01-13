a173f392a06aabeb9a57665b6f88447b8b7a2ed9
Jalyn Campbell <69061083+JCyberAI@users.noreply.github.com>
2021-01-12 21:48:29 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+// TODO: Include packages needed for this application
+
+// TODO: Create an array of questions for user input
+const questions = [];
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
+
+
+/* Create a gitignore file and type 'node_modules' on the inside. 
+So you dont commit your modules to github */
