df919b3fc5cb98106e73d77b44b60aa5e7ffdc37
ArnoldIngrave <jalyn.campbell@yahoo.com>
2021-01-15 20:24:45 -0500

    commit on main

--- index.js
+++ index.js
@@ -4,19 +4,28 @@ const fs = require("fs");
 const util = require("util");
 
 // TODO: Create an array of questions for user input
-const questions = {
-
-    type: "input",
-    name: "name",
-    message: "What is your middle name?"
-
+const questions = () => {
+    return inquirer.prompt ([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your middle name?"
+        }
+    ])    
 }
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const writeToFile = (data) => {
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = async () => {
+
+    const data = await questions();
+    console.log(data);
+
+}
 
 // Function call to initialize app
 init();
