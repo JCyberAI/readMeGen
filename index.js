22e4e3ebae715d4521a8364f637efdb8bfd84216
ArnoldIngrave <jalyn.campbell@yahoo.com>
2021-01-15 19:22:52 -0500

    commit on main

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = {
+
+    type: "input",
+    name: "name",
+    message: "What is your middle name?"
+
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
